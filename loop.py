5d470d6cecf9ebb3cd98f29fbf3066669ff52867
RaviKantBadola <ravi.rock7552@gmail.com>
2024-07-15 19:05:29 +0530

    add

---
+++ loop.py
@@ -0,0 +1,18 @@
+import math
+
+for i in range(1, 11):
+    print("mahesh")
+
+counter = 1
+while counter <= 10:
+    print("a", counter * 2)
+    counter = counter + 1
+
+
+def add(a, b):
+    c = a + b
+    return c
+
+
+print(add(1 ,5))
+
---
+++ range.py
@@ -0,0 +1,6 @@
+n=list(range(2,11,2))
+print(n)
+
+#start value:2
+#end value:11
+#step:2
